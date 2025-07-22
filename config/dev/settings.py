d289f556926df1c14f33b5839725c32c976ec1cf
Jeremy Bowers <jeremyjbowers@gmail.com>
2025-07-21 20:27:34 -0400

    getting ready for production

--- config/dev/settings.py
+++ config/dev/settings.py
@@ -241,7 +241,6 @@ STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY', default=None)
 STRIPE_PUBLISHABLE_KEY = env('STRIPE_PUBLISHABLE_KEY', default=None)
 STRIPE_WEBHOOK_SECRET = env('STRIPE_WEBHOOK_SECRET', default=None)
 
-
 STRIPE_PRICE_ID = env('STRIPE_PRICE_ID', default=None)
 
 # Subscription pricing
