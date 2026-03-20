067065d7d5b894ee09312c65cca79a3f3bdd622a
Jeremy Bowers <jeremyjbowers@gmail.com>
2026-03-20 09:42:41 -0400

    let's get this up to the server so I don't have to keep running this all by hand.

--- overslot/management/commands/live_update.py
+++ overslot/management/commands/live_update.py
@@ -1,4 +1,3 @@
-import time
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.core.management import call_command
@@ -17,14 +16,7 @@ class Command(BaseCommand):
         call_command('load_college_pitchers')
         call_command('load_hs_hitters')
         call_command('generate_player_duplicates')
-        
-        # Load 643 stats and games only every 4 hours (6 times per day)
-        # Only run if we're in the first 5 minutes of the 4-hour interval
-        current_time = int(time.time())
-        interval_seconds = 4 * 60 * 60  # 4 hours
-        if (current_time % interval_seconds) < 300:
-            call_command('load_games')
-            call_command('load_coaches_poll')
-            call_command('load_643_stats')
-        else:
-            self.stdout.write("Skipping 643 stats and games update (not in update window - runs every 4 hours)")
+
+        call_command('load_643_stats')
+        call_command('load_games')
+        call_command('load_coaches_poll')
